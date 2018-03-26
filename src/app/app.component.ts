267b5b0cc5a601c98884d1365fadc2581e9b3155
Hrvoje G <hrvoje.grdic1@gmail.com>
2018-03-26 16:44:52 +0200

    example app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,24 @@
 import {Component} from '@angular/core';
-import {Todo} from './todo';
-import {TodoDataService} from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [TodoDataService]
+  providers: []
 })
 export class AppComponent {
 
-  newTodo: Todo = new Todo();
+  public newTodo = {};
+  public todos = [];
 
-  constructor(private todoDataService: TodoDataService) {
+  constructor() {
   }
 
   addTodo() {
-    this.todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
   }
 
   toggleTodoComplete(todo) {
-    this.todoDataService.toggleTodoComplete(todo);
   }
 
-  removeTodo(todo) {
-    this.todoDataService.deleteTodoById(todo.id);
-  }
-
-  get todos() {
-    return this.todoDataService.getAllTodos();
-  }
 
 }
